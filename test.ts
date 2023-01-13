5382ff2470af9161addf5ed13b386dbb31722163
manyxu <manyxu@foxmail.com>
2023-01-13 19:30:52 +0800

    change names

--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@
 
 manykit.connectIrReceiver(DigitalPin.P0, IrProtocol.NEC);
 manykit.onIrButton(IrButton.OK, IrButtonAction.Pressed, () => {});
-manykit.onIrButton(IrButton.Up, IrButtonAction.Released, () => {});
+manykit.onIrButton(IrButton.Add, IrButtonAction.Released, () => {});
 manykit.onIrDatagram(() => {});
 const received: boolean = manykit.wasIrDataReceived();
 const button: number = manykit.irButton();
